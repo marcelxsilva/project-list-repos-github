bc718fbe06bf877b7cfb0172892368bafa042326
Marcelo Silva <maarcelomonteiro@hotmail.com>
2019-10-31 16:56:52 -0300

    Feature: charging data form API

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import api from '../../services/api';
 
 class Repository extends React.Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true
+  }
   async componentDidMount() {
     const { match } = this.props;
     const repoName = decodeURIComponent(match.params.repository);
@@ -15,7 +20,12 @@ class Repository extends React.Component {
         }
       })
     ]);
-    console.log(issues)
+    console.log({ issues, repository })
+    this.setState({
+      loading: false,
+      repository: repository.data,
+      issues: issues.data
+    })
   }
 
   render() {
